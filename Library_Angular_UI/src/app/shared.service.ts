bd897f0584f1d96522d0d3165a2674ac8fa930d5
{Mbhole@123} <{mayur@rhythmflows.com}>
2022-10-03 22:43:27 +0530

    routing changes

--- Library_Angular_UI/src/app/shared.service.ts
+++ Library_Angular_UI/src/app/shared.service.ts
@@ -4,28 +4,31 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http'
 import { Observable, throwError } from 'rxjs'
 import { environment } from 'src/environments/environment';
 
+// let APIUrl = "http://127.0.0.1:8000"
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class SharedService {
-  [x: string]: any;
+  // [x: string]: any;
   
-readonly APIUrl = "http://127.0.0.1:8000";
+  // constructor(private http:HttpClient) { }
 
-  constructor(private http:HttpClient) { }
-
-  getBookData():Observable<any[]>{
-    return this.http.get<any[]>(this.APIUrl + '/book/');
-  }
-
-  // getBookData() {
-  //   return this.http.get<any[]>(this.APIUrl + 'book/');
-  //   // return this.http.get(`${environment.apiUrl}`.concat('/book/'));
+  // getBookData():Observable<any[]>{
+  //   return this.http.get<any[]>(this['APIUrl'] + '/book/');
+    
   // }
+  constructor(  private http : HttpClient
+    ) { }
+
+  getBookData() {
+    return this.http.get<any[]>(`${environment.apiUrl}`.concat('book/'));
+  }
 
  saveBookData(value:any) {
   if (value.id) {
-    return this.http.put<any[]>(this.APIUrl.concat('/book/' + value.id + '/'), value)
+    return this.http.put<any[]>(`${environment.apiUrl}`.concat('book/' + value.id + '/'), value)
       .pipe(map(data => {
         var status=1
         data[status] = 1;
@@ -34,7 +37,7 @@ readonly APIUrl = "http://127.0.0.1:8000";
         catchError(this.errorHandler));
   }
   else {
-    return this.http.post<any[]>(this.APIUrl.concat('/book/'), value)
+    return this.http.post<any[]>(`${environment.apiUrl}`.concat('book/'), value)
       .pipe(map(data => {
         var status=2
         data[status] = 2;
@@ -46,12 +49,12 @@ readonly APIUrl = "http://127.0.0.1:8000";
   }
 
   deleteBookData(id:any) {
-    return this.http.delete<any[]>(this.APIUrl.concat(`/book/${id}/`));
+    return this.http.delete<any[]>(`${environment.apiUrl}`.concat(`book/${id}/`));
   }
 
   
   gotoLogin(value:any):Observable<any[]>{
-    return this.http.post<any[]>(this.APIUrl.concat('/login/'), value)
+    return this.http.post<any[]>(`${environment.apiUrl}`.concat('login/'), value)
       .pipe(map(data => {
         return data;
       }),
@@ -60,7 +63,7 @@ readonly APIUrl = "http://127.0.0.1:8000";
   }
 
   gotoSignup(value:any):Observable<any[]>{
-    return this.http.post<any[]>(this.APIUrl.concat('/signup/'), value)
+    return this.http.post<any[]>(`${environment.apiUrl}`.concat('signup/'), value)
       .pipe(map(data => {
         return data;
       }),
--- Library_Angular_UI/src/environments/environment.ts
+++ Library_Angular_UI/src/environments/environment.ts
@@ -2,10 +2,14 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// export const environment = {
+//   production: false
+// };
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://127.0.0.1:8000/'
+  // apiUrl: 'https://recontest.rhythm.works:8000'
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
